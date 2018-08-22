bf26dedb4456bb065986de2652e41d17cff61670
Jose Luis Eiguren <joseluiseiguren@gmail.com>
2018-08-22 21:31:39 +0200

    init

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+EndPoints
+	Version 1
+		http://localhost:60388/api/persons
+		http://localhost:60388/api/persons?api-version=1.0
+	Version 2
+		http://localhost:60388/api/Persons?api-version=2.0
+
+Packages NuGet
+	Microsoft.AspNet.WebApi.Versioning
---
+++ webapi_netframework_versioning_querystring/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace webapiexample
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"] = typeof( ApiVersionRouteConstraint )
+                }
+            };
+
+            config.MapHttpAttributeRoutes(constraintResolver);
+
+            // si no se especifica ruta, se toma la default (que es la 1.0)
+            config.AddApiVersioning(o =>
+            {
+                o.AssumeDefaultVersionWhenUnspecified = true;
+                o.ReportApiVersions = true;
+                o.DefaultApiVersion = ApiVersion.Default;
+            });            
+        }
+    }
+}
